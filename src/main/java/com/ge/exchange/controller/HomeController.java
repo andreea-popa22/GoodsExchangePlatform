b55ab9e306553042f823a52be3c8f97b28e55b73
Andreea <andreea_popa22@yahoo.ro>
2023-01-26 00:57:50 +0200

    user auth in progress

--- src/main/java/com/ge/exchange/controller/HomeController.java
+++ src/main/java/com/ge/exchange/controller/HomeController.java
@@ -1,18 +1,27 @@
 package com.ge.exchange.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping
 public class HomeController {
     @GetMapping("/")
-    public String displayWelcome(){
-        return "Welcome";
+    public String displayLogin(Model model){
+        model.addAttribute("text", "log in");
+        return "login";
     }
 
     @GetMapping("/home")
-    public String displayHome(){
-        return "Home";
+    public String displayHome(Model model){
+        return "home";
+    }
+
+    @GetMapping("/hello")
+    public String displayHello(Model model){
+        return "hello";
     }
 
 }
--- src/main/java/com/ge/exchange/model/Post.java
+++ src/main/java/com/ge/exchange/model/Post.java
@@ -1,8 +1,17 @@
 package com.ge.exchange.model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
+@Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "POST")
 public class Post {
@@ -14,65 +23,4 @@ public class Post {
     private String date;
     private String content;
     private int authorId;
-
-    public Post() {
-
-    }
-
-    public Post(int id, String title, String category, String date, String content, int authorId) {
-        this.id = id;
-        this.title = title;
-        this.category = category;
-        this.date = date;
-        this.content = content;
-        this.authorId = authorId;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getCategory() {
-        return category;
-    }
-
-    public void setCategory(String category) {
-        this.category = category;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public void setDate(String date) {
-        this.date = date;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
-
-    public int getAuthorId() {
-        return authorId;
-    }
-
-    public void setAuthorId(int authorId) {
-        this.authorId = authorId;
-    }
 }
